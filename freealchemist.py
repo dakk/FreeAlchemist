5731389d44c33b4b84a8a7529583c2695509032b
dakk <gessadavide@gmail.com>
2014-01-25 23:20:11 +0100

    rilicensing

--- freealchemist.py
+++ freealchemist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # FreeAlchemist (by Dak [dak.linux@gmail.com])
 """
-Copyright (C) 2008 Dak
+Copyright (C) 2008-2014 Davide Gessa
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
